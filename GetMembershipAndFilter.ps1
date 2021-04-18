51d6e1a0099324909ff9c8bb4d795cac25533cbc
SethBurdin <60669235+SethBurdin@users.noreply.github.com>
2021-04-18 11:40:59 -0500

    Update GetMembershipAndFilter.ps1

--- GetMembershipAndFilter.ps1
+++ GetMembershipAndFilter.ps1
@@ -1,29 +1,43 @@
 $List=$null
-$group=$null
+#$group=$null
 $getmembervar=$null
 $filteredresults=$null
 
-[System.Collections.ArrayList]$List = @()
-$input= get-content ingestion.csv
+[System.Collections.ArrayList]$List = @(
+                                        Group = $List[0]
+                                        AllMembers = @($List[1] | Out-String).Trim() 
+                                        FilteredResult = @($List[2]  | Out-String).Trim()  
+                                        )
+
 $output="results.txt"
 
 
-foreach($_ in $input) {
-
+ foreach($_ in get-content ingestion.csv) {
+write-output $_
+<#
 $arrline= $_.split(",")
-$group= $_.
-$getmembervar=(Get-DistributionGroupMember -identity $group ).PrimarySmtpAddress
-$List.Add($group)
-$filteredresults= $getmembervar | where {_.PrimarySmtpAddress -match "@filter1" -or "@filter2"}
+$group= $arrline[0]
+#>
+
+$getmembervar=(Get-DistributionGroupMember -identity $_ ).PrimarySmtpAddress
+$List.Add($_)
+$filteredresults=$getmembervar  # | where {_.PrimarySmtpAddress -match "@filter1" -or "@filter2"}
 $List.Add($filteredresults)
+$List.Add($getmembervar)
 
-##variable sanitation 
-$group=$null
+<##variable sanitation 
+#$group=$null
 $filteredresults=$null
+#>
 
-
-$List -join "," | tee-object -filepath $output
+<#
+write-output "$List.$_" + "$List.$getmembervar" + "$List.$filteredresults"  
+"$List.$_" + "$List.$getmembervar" + "$List.$filteredresults"  -join "," | out-file -filepath $output
+$List[0] + $List[1] + $List[2]  -join "," | out-file -filepath longshot.txt
+-join "," 
+#>
+$List | export-csv  test2.csv -NoTypeInformation
+$List -join ","  | out-file -filepath $output
 
 }
-
 write-output $List | fl
