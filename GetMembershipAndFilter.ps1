7c1f581159e73dca52a148b4bbee23b0d6c7287b
SethBurdin <60669235+SethBurdin@users.noreply.github.com>
2021-04-17 21:46:42 -0500

    Update GetMembershipAndFilter.ps1

--- GetMembershipAndFilter.ps1
+++ GetMembershipAndFilter.ps1
@@ -5,16 +5,16 @@ $filteredresults=$null
 
 [System.Collections.ArrayList]$List = @()
 $input= get-content ingestion.csv
-$output=results.txt
+$output="results.txt"
 
 
 foreach($_ in $input) {
 
 $arrline= $_.split(",")
-$group= $_.[0]
-$getmembervar=(Get-DistributionGroupMember -identity $group[0] ).PrimarySTMPAddress
+$group= $_.
+$getmembervar=(Get-DistributionGroupMember -identity $group ).PrimarySmtpAddress
 $List.Add($group)
-$filteredresults= $getmembervar | where {_.PrimarySTMPAddress -match "@filter1" -or "@filter2"}
+$filteredresults= $getmembervar | where {_.PrimarySmtpAddress -match "@filter1" -or "@filter2"}
 $List.Add($filteredresults)
 
 ##variable sanitation 
